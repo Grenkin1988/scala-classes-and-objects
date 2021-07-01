8d5d0d72e417a6f6cde5ebaa024aa11f05fdc077
Grenkin1988 <kruglovgleb@gmail.com>
2021-07-01 20:43:56 +0200

    abstract

--- src/main/scala/Account.scala
+++ src/main/scala/Account.scala
@@ -1,10 +1,11 @@
 import java.time.LocalDateTime
 import java.util.UUID
 
-class Account(id: UUID, name: String, dateOpened: LocalDateTime) {
+class OldAccount(id: UUID, name: String, dateOpened: LocalDateTime) {
     private val _id: UUID = id
     private val _name: String = name
     private val _dateOpened: LocalDateTime = dateOpened
+    private val _accountType = "Credit"
 
     def this(name: String){
         this(UUID.randomUUID(), name, LocalDateTime.now)
@@ -15,28 +16,42 @@ class Account(id: UUID, name: String, dateOpened: LocalDateTime) {
     def getDateOpened = _dateOpened
 }
 
-class CreditAccount(name: String) extends Account(name: String){
-    private val _accountType = "Credit"
+abstract class Account(id: UUID, name: String, dateOpened: LocalDateTime) {
+    private val _id: UUID = id
+    private val _name: String = name
+    private val _dateOpened: LocalDateTime = dateOpened
+    val _accountType: String
+
+    def this(name: String){
+        this(UUID.randomUUID(), name, LocalDateTime.now)
+    }
+
+    def getId = _id
+    def getName = _name
+    def getDateOpened = _dateOpened
     def getAccountType: String = _accountType
+}
+
+class CreditAccount(name: String) extends Account(name: String){
+    override val _accountType = "Credit"
 }
 
 class DepositAccount(name: String) extends Account(name: String){
-    private val _accountType = "Deposit"
-    def getAccountType: String = _accountType
+    override val _accountType = "Deposit"
 }
 
 object AccountRunner extends App{
-    val a1 = new Account(UUID.randomUUID(), "Account 01", LocalDateTime.now)
-    val a2 = new Account(UUID.randomUUID(), "Account 02", LocalDateTime.now.plusHours(6))
-    val a3 = new Account("Account 3")
+    val a1 = new OldAccount(UUID.randomUUID(), "Account 01", LocalDateTime.now)
+    val a2 = new OldAccount(UUID.randomUUID(), "Account 02", LocalDateTime.now.plusHours(6))
+    val a3 = new OldAccount("Account 3")
 
     println(a1.getId, a1.getName, a1.getDateOpened)
     println(a2.getId, a2.getName, a2.getDateOpened)
     println(a3.getId, a3.getName, a3.getDateOpened)
     println("--------------------------------------------------")
 
-    val ca1 = new CreditAccount("Credit Accout 01")
+    val ca1: Account = new CreditAccount("Credit Accout 01")
     println(ca1.getId, ca1.getName, ca1.getDateOpened, ca1.getAccountType)
-    val da1 = new DepositAccount("Deposit Accout 01")
+    val da1: Account = new DepositAccount("Deposit Accout 01")
     println(da1.getId, da1.getName, da1.getDateOpened, da1.getAccountType)
 }
