58f708005ed708fe7ad41e64bcd20acdea584d34
Grenkin1988 <kruglovgleb@gmail.com>
2021-07-02 15:49:18 +0200

    internal function

--- src/main/scala/com/bonsaiilabs/entities/User.scala
+++ src/main/scala/com/bonsaiilabs/entities/User.scala
@@ -1,3 +1,5 @@
 package com.bonsaiilabs.entities
 
-case class User(val first: String, val last: String)
+case class User(val first: String, val last: String) {
+    private[entities] def getId: String = ???
+}
