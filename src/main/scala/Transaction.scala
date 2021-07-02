0a40033e8f32f60fcf8cd1d83e2dfaeb097bc08a
Grenkin1988 <kruglovgleb@gmail.com>
2021-07-02 13:41:36 +0200

    declair self type

--- src/main/scala/Transaction.scala
+++ src/main/scala/Transaction.scala
@@ -3,19 +3,18 @@ trait User {
     def getLast: String
 }
 
-trait Transactio extends User {
+trait Transactio {
+    this: User =>
     def printAmountWithTransaction(amount: Double) = {
-        val fullCustomerName = getFirst + " " + getLast
+        val fullCustomerName = this.getFirst + " " + this.getLast
         val tax = amount * 0.10 // 10%
         println(s"$fullCustomerName paid $tax on amount of $amount")
     }
 }
 
-class DebitTransaction(val first: String, val last: String) extends Transactio {
+class DebitTransaction(val first: String, val last: String) extends User with Transactio {
   override def getFirst: String = first
   override def getLast: String = last
-
-
 }
 
 object TransactionRunner extends App {
