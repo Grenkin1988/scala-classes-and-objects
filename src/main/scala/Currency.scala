4cd2fb4c4e163707dc2e2089cf9429609846baed
Grenkin1988 <kruglovgleb@gmail.com>
2021-07-02 12:52:28 +0200

    add apply

--- src/main/scala/Currency.scala
+++ src/main/scala/Currency.scala
@@ -1,6 +1,9 @@
 object Currency {
     private val conversionTable: Map[String, Double] =
         Map("CAD" -> 1 / 1.3, "NZD" -> 1 / 1.5)
+
+    // With apply no need for new key word for object creation
+    def apply(code: String, amount: Double) = new Currency(code, amount)
 }
 class Currency(code: String, amount: Double) {
     import Currency.conversionTable
@@ -21,12 +24,12 @@ class Currency(code: String, amount: Double) {
 }
 
 object CurrencyRunner extends App {
-    val usd1 = new Currency("USD", 100.12)
+    val usd1 = Currency("USD", 100.12)
     println(usd1)
 
-    val nzd1 = new Currency("NZD", 100.12)
+    val nzd1 = Currency("NZD", 100.12)
     println(nzd1)
 
-    val cad1 = new Currency("CAD", 100.12)
+    val cad1 = Currency("CAD", 100.12)
     println(cad1)
 }
