3817015d3a2212301f43a99d533100ea5f6e73d8
Grenkin1988 <kruglovgleb@gmail.com>
2021-07-02 12:56:51 +0200

    move function

--- src/main/scala/Currency.scala
+++ src/main/scala/Currency.scala
@@ -3,26 +3,26 @@ object Currency {
         Map("CAD" -> 1 / 1.3, "NZD" -> 1 / 1.5)
 
     // With apply no need for new key word for object creation
-    def apply(code: String, amount: Double) = new Currency(code, amount)
+    def apply(code: String, amount: Double) = new Currency(code, amount, getCostInUsd(code, amount))
 
     def createUSD(amount: Double) = apply("USD", amount)
     def createNZD(amount: Double) = apply("NZD", amount)
     def createCAD(amount: Double) = apply("CAD", amount)
-}
-class Currency(code: String, amount: Double) {
-    import Currency.conversionTable
-    private val _code = code
-    private val _amount = amount
-    private val _inUSD: Double = getCostInUsd
 
-    private def getCostInUsd: Double = {
-        _code match {
-            case "USD" => _amount
+    private def getCostInUsd(code: String, amount: Double): Double = {
+        code match {
+            case "USD" => amount
             case ccy if conversionTable.contains(ccy) =>
-                _amount * conversionTable(ccy)
+                amount * conversionTable(ccy)
             case _ => throw new IllegalArgumentException(s"No conversion available for ${_code}")
         }
     }
+}
+class Currency(code: String, amount: Double, inUSD: Double) {
+    import Currency.conversionTable
+    private val _code = code
+    private val _amount = amount
+    private val _inUSD = inUSD
 
     override def toString(): String = s"${_code} ${_amount} = USD ${_inUSD}"
 }
